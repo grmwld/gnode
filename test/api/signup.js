9639809ca9f67862b894b0c01525a6cb22b0c1a7
Alexis GRIMALDI <alexis.grimaldi@gmail.com>
2012-12-31 10:13:30 +0100

    Add test for user creation upon signing up

--- test/api/signup.js
+++ test/api/signup.js
@@ -10,47 +10,53 @@ var expect = require('chai').expect
   
 describe('POST /signup', function() {
 
-  it('redirects to /dashboard if the form is valid', function(done) {
-    request(app)
-      .post('/login')
-      .send({
-        username: 'agrimaldi',
-        password: 'qwe'
-      })
-      .expect(200)
-      .end(function(err, res) {
-        expect(err).to.not.exist;
-        expect(res.body).to.have.property('retStatus', 'success');
-        expect(res.body).to.have.property('user');
-        expect(res.body.user).to.have.keys(['__v', 'bookmarks',
-          'username', 'password', 'admin', 'email', '_id'
-        ]);
-        done();
-      });
+  var valid_form = {
+    username: 'agrimaldi',
+    password: 'qwe',
+    password_confirm: 'qwe',
+    email: 'agrimaldi@gstrider.org'
+  };
+  var invalid_form = {
+    username: 'agrimaldi',
+    password: 'qwe',
+    password_confirm: 'lskd',
+    email: ''
+  };
+
+
+  describe('with valid form', function() {
+
+    it('redirects to /dashboard if the form is valid', function(done) {
+      request(app)
+        .post('/signup')
+        .send(valid_form)
+        .expect(302)
+        .end(function(err, res) {
+          expect(err).to.not.exist;
+          expect(res.header.location).to.include('/dashboard');
+          done();
+        });
+    });
+    it('should create a new User', function() {
+      request(app)
+        .post('/signup')
+        .send(valid_form)
+        .expect(302)
+        .end(function(err, res) {
+          expect(err).to.not.exist;
+          User.find(function(err, users) {
+            expect(err).to.not.exist;
+            expect(users).to.have.length(1);
+            var user = users[0];
+            expect(user).to.have.property('username', 'agrimaldi');
+            expect(user).to.have.property('email', 'agrimaldi@gstrider.org');
+            expect(user).to.have.property('passwordHash');
+            done();
+          });
+        });
+    });
+    
   });
 
-  it('reponds infavorabily to login attempt', function(done) {
-    request(app)
-      .post('/login')
-      .send({
-        username: 'agrimaldi',
-        password: 'qweasda'
-      })
-      .expect({
-        retStatus: 'failure',
-      }, done);
-  });
-
-  it('reponds infavorabily to login attempt', function(done) {
-    request(app)
-      .post('/login')
-      .send({
-        username: 'non-existent',
-        password: 'qwe'
-      })
-      .expect({
-        retStatus: 'failure',
-      }, done);
-  });
 
 });
